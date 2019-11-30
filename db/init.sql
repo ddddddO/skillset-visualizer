15a506dff75ea73a41cc0fbade41034fc406ea06
ddddddO <lbfdeatq@gmail.com>
2019-11-30 21:16:29 +0900

    add initialize DB sql

---
+++ db/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE skills (
+	id serial PRIMARY KEY,
+	categories json NOT NULL
+);
+
+INSERT INTO skills(categories) VALUES ('{"frontend": 1,"backend": 2,"database": 3,"infrastructure": 3,"network": 2,"facilitation": 4}');
