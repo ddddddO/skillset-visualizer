9badf534284b34b71a776a126770021c74cb4623
ddddddO <lbfdeatq@gmail.com>
2019-11-30 21:54:24 +0900

    apiからDB接続

--- api/main.go
+++ api/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
+
+const DBDSN = "user=postgres dbname=skillsets host=localhost port=5432 sslmode=disable"
+
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("postgres", DBDSN)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	log.Print("start api server")
@@ -25,6 +40,23 @@ func fetchGraphDataHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+
+	rows, err := db.Query("SELECT * FROM skills")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var (
+		id         int64
+		categories string
+	)
+	for rows.Next() {
+		if err := rows.Scan(&id, &categories); err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("ID: %d, CATEGORIES: %s", id, categories)
+	}
 
 	// あとで、構造体に入れるようにしてマーシャル対応
 	body := `{"frontend": 1,"backend": 2,"database": 3,"infrastructure": 3,"network": 2,"facilitation": 4}`
