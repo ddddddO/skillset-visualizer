e6bf96f9f21be22590a64b7135fa03156ca286ff
ddddddO <lbfdeatq@gmail.com>
2019-12-08 04:30:43 +0900

    gke tf

---
+++ deployments/terraform/gke/main.tf
@@ -0,0 +1,51 @@
+provider google {
+  credentials = "${file("~/.config/gcloud/legacy_credentials/lbfdeatq@gmail.com/adc.json")}"
+
+  project = "work1111"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+# ネットワーク
+resource google_compute_network work_net {
+  name = "work-net"
+}
+
+resource google_container_cluster gcc {
+  name = "work-gcc"
+  location = "us-central1"
+  network = "${google_compute_network.work_net.self_link}" 
+
+  remove_default_node_pool = true
+  initial_node_count = 1
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = true
+    }
+  }
+}
+
+resource google_container_node_pool gc_node_pool {
+  name = "gc-node-pool"
+  location   = "us-central1"
+  cluster = "${google_container_cluster.gcc.name}"
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
