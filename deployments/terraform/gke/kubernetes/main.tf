71f91ccfbf83775880bb1a0a3a1900219eff5ae9
ddddddO <lbfdeatq@gmail.com>
2019-12-08 05:06:08 +0900

    gke/kube nginx pod

--- deployments/terraform/gke/kubernetes/main.tf
+++ deployments/terraform/gke/kubernetes/main.tf
@@ -1,2 +1,24 @@
 provider kubernetes {
 }
+
+# WIP
+# https://www.terraform.io/docs/providers/kubernetes/guides/getting-started.html
+resource "kubernetes_pod" "nginx" {
+  metadata {
+    name = "nginx-example"
+    labels = {
+      App = "nginx"
+    }
+  }
+
+  spec {
+    container {
+      image = "nginx:1.7.8"
+      name  = "example"
+
+      port {
+        container_port = 80
+      }
+    }
+  }
+}
