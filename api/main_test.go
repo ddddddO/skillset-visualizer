0867ab2bc868c09f652693b4871fca2e0e1e5c85
ddddddO <lbfdeatq@gmail.com>
2020-01-01 00:49:29 +0900

    add fetchGraphDataHandler test

--- api/main_test.go
+++ api/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,6 +9,30 @@ import (
 
 // ref: https://devblog.thebase.in/entry/2018/12/04/110000
 func TestHealthCheckHandler(t *testing.T) {
+	tests := []struct {
+		method             string
+		expectedStatusCode int
+	}{
+		{http.MethodGet, http.StatusOK},
+		{http.MethodPost, http.StatusMethodNotAllowed},
+		{http.MethodDelete, http.StatusMethodNotAllowed},
+	}
+
+	for _, tt := range tests {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(tt.method, "/health", nil)
+
+		healthCheckHandler(w, r)
+		res := w.Result()
+		defer res.Body.Close()
+
+		if res.StatusCode != tt.expectedStatusCode {
+			t.Errorf("expected status code is %d, got %d", tt.expectedStatusCode, res.StatusCode)
+		}
+	}
+}
+
+func TestFetchGraphDataHandler(t *testing.T) {
 	tests := []struct {
 		method             string
 		expectedStatusCode int
